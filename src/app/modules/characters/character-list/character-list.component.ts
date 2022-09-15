8cfcbe4ba4aef3081178ea44c3980b185b7a78ef
Checo <checo135@hotmail.com>
2022-09-15 11:48:32 -0500

    Character Pagination & Search -- Styles

--- src/app/modules/characters/character-list/character-list.component.ts
+++ src/app/modules/characters/character-list/character-list.component.ts
@@ -14,13 +14,32 @@ export class CharacterListComponent implements OnInit {
   //Data
   listCharacters$: Observable<ResponseAPI>;
   apiURL: string = environment.apiCharacter;
+  searchedCharacter: string = '';
 
+  /**
+   * The constructor function is a special function that is called when a new instance of the class is
+   * created
+   * @param {CharacterService} characterService - This is the service that we created earlier.
+   */
   constructor(private characterService: CharacterService) {
     this.listCharacters$ = this.characterService.getCharacters(this.apiURL);
   }
   ngOnInit(): void {}
 
+  /**
+   * This function takes a parameter of type Character and prints it to the console.
+   * @param {Character} character - Character
+   */
   printCharacter(character: Character) {
     console.log(character);
+  }
+
+  /**
+   * If the url parameter is null, then set the url variable to the apiURL variable
+   * @param {string | null} url - string | null
+   */
+  nextPage(url: string | null) {
+    url = url ? url : this.apiURL;
+    this.listCharacters$ = this.characterService.getCharacters(url);
   }
 }
--- src/app/shared/components/character-card/character-card.component.ts
+++ src/app/shared/components/character-card/character-card.component.ts
@@ -13,14 +13,26 @@ export class CharacterCardComponent implements OnInit {
   characterCopy!: Character;
   constructor() {}
 
+  /**
+   * It resets the character to the original character
+   */
   resetCharacter() {
     this.character = this.characterCopy;
   }
 
+  /**
+   * We're creating a copy of the character object, so that we can compare the original character
+   * object to the copy
+   */
   ngOnInit() {
     this.characterCopy = JSON.parse(JSON.stringify(this.character));
   }
 
+  /**
+   * The function takes a character as an argument, and emits the character to the printCharacterEvent
+   * @param {Character} character - Character - This is the character that we want to pass to the
+   * parent component.
+   */
   printCharacter(character: Character) {
     this.printCharacterEvent.emit(character);
   }
